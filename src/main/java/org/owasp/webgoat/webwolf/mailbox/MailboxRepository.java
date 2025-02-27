99c7f13ca1043c909953a30a90fbd42bdf2a50f0
Mageitor <162612586+Mageitor@users.noreply.github.com>
2025-02-27 18:00:17 +0100

    Update MailboxRepository.java

--- src/main/java/org/owasp/webgoat/webwolf/mailbox/MailboxRepository.java
+++ src/main/java/org/owasp/webgoat/webwolf/mailbox/MailboxRepository.java
@@ -25,11 +25,8 @@ package org.owasp.webgoat.webwolf.mailbox;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * @author nbaars
- * @since 8/17/17.
- */
-public interface MailboxRepository extends JpaRepository<Email, String> {
-
-  List<Email> findByRecipientOrderByTimeDesc(String recipient);
+public interface MailboxRepository extends JpaRepository<Email, Long> {
+    List<Email> findByRecipientOrderByTimeDesc(String recipient);
+    void deleteByRecipient(String recipient);
 }
+
