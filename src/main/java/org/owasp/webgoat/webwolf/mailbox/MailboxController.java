7062bb88ee37e7ac5f23a87f84fd8411dc3e44d7
Mageitor <162612586+Mageitor@users.noreply.github.com>
2025-02-27 18:06:30 +0100

    Update MailboxController.java

--- src/main/java/org/owasp/webgoat/webwolf/mailbox/MailboxController.java
+++ src/main/java/org/owasp/webgoat/webwolf/mailbox/MailboxController.java
@@ -35,15 +35,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import java.util.List;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-import jakarta.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
